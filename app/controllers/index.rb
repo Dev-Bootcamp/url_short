405b7f01637237972ae7881a8c8e8f878aa41224
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 10:17:25 -0500

    third

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,16 +4,28 @@ get '/' do
   erb :index
 end
 
+get "/create_url" do
+
+  erb :create_url 
+end
+
+
 get '/:id' do
   params[:id]
   @url = Url.where(alter: params[:id])
   @real = @url.first
-  # p @real.website
-  # redirect to ("#{@real.website}")
-end  
+  p @real.website
+  redirect to ("http://#{@real.website}")
+end
 
-get "/create_url" do
+post '/create_url' do
+  p params
+  p params[:url]
+  @url = Url.new(params[:url])
+  @url.newurl
+  @url.save
+  redirect to ('/')
+end
 
-  erb :create_url 
-end  
+
 
